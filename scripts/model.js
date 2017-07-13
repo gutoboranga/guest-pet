9c9fcb8e1bb80a94994d8641df1e103858c1e801
Augusto Boranga <guto.boranga@gmail.com>
2017-07-13 17:51:10 -0300

    arruma os models

--- scripts/model.js
+++ scripts/model.js
@@ -1,9 +1,56 @@
 class User {
-	constructor(nome, email, senha, city) {
+	constructor(nome, email, senha, city, birthDate, isHostUser, isOwnerUser, isFiscalUser) {
 		this.nome = nome;
 		this.email = email;
 		this.senha = senha;
+		this.birthDate = birthDate;
+		
+		this.isHostUser = isHostUser;
+		this.isOwnerUser = isOwnerUser;
+		this.isFiscalUser = isFiscalUser;
+		
+		// owner attributes
+		this.pets = pets;
+		this.ownerHistory = ownerHistory;
+		this.ownerPoints = ownerPoints;
+		
+		// host attributes
+		this.homes = homes;
+		this.hostHistory = hostHistory;
+		this.hostPoints = hostPoints;
+	}
+}
+
+class Pet {
+	constructor(name, species, size, nature, state, photo, birthDate) {
+		this.name = name;
+		this.species = species;
+		this.size = size;
+		this.nature = nature;
+		this.state = state;
+		this.birthDate = birthDate || 0;
+		this.photo = photo;
+	}
+}
+
+class Home {
+	constructor(name, adress, photos, capacity, currentOccupation, description) {
+		this.name = name;
+		this.adress = adress;
+		this.photos = photos;
+		this.capacity = capacity;
+		this.currentOccupation = currentOccupation;
+		this.description = description;
+	}
+}
+
+class Adress {
+	constructor(street, number, city, state, country) {
+		this.street = street;
+		this.number = number;
 		this.city = city;
+		this.state = state;
+		this.country = country;
 	}
 }
 
