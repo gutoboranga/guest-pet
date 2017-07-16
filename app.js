a658ab3b829c20ab2ef88340cb097f46d5c9fb2e
Augusto Boranga <guto.boranga@gmail.com>
2017-07-16 05:45:08 -0300

    + heroku

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
-// app.use(express.static(__dirname + '/templates'));
+app.use(express.static(__dirname + '/templates'));
 app.use(express.static(__dirname + '/'));
 
 app.get('/', function(request, response) {
