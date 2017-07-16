f9b9d49c99bbf4b857e439060a717076d5141cb9
Augusto Boranga <guto.boranga@gmail.com>
2017-07-16 05:10:27 -0300

    + herkoukdk

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ var app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
+app.use(express.static('templates'));
+app.use(express.static('stylesheets'));
+app.use(express.static('scripts'));
+app.use(express.static('images'));
+app.use(express.static('model'));
+
 app.get('/', function(request, response) {
   response.render('templates/index.html');
 });
